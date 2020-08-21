0d018c2a0d327967fa25304d2792073f6b1bfb03
Jeff Glanville <jeff.glanville@outlook.com>
2020-08-21 01:34:41 -0500

    update

--- seeds/fruits.js
+++ seeds/fruits.js
@@ -1,5 +1,7 @@
 
 exports.seed = async function(knex) {
+  await knex("fruits").truncate
+
   await knex("fruits").insert([
     { name: "dragon fruit", avgWeightOz: 16.7, delicious: true, color: "red" },
 		{ name: "durian", avgWeightOz: 52.9, delicious: false, color: "yellow" },
